c3e5e6881f8d6a3969b5d8c748dd46b692be6e34
Neyza17 <nesyaratuanastasya@gmail.com>
2025-02-20 20:26:38 +0700

    first commit

--- MahasiswaMain17.java
+++ MahasiswaMain17.java
@@ -1,6 +1,6 @@
 public class MahasiswaMain17 {
     public static void main(String[] args) {
-        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa17 mhs1 = new Mahasiswa17();
         
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720117";
@@ -11,30 +11,5 @@ public class MahasiswaMain17 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
-    }
-}
-class Mahasiswa {
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
-
-    void tampilkanInformasi() {
-        System.out.println("Nama : " + nama);
-        System.out.println("NIM  : " + nim);
-        System.out.println("Kelas: " + kelas);
-        System.out.println("IPK  : " + ipk);
-    }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
     }
 }
