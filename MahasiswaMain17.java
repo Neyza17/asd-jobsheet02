07662b3e127b2cdcd7b4b98937b2bec3d913f9bb
Neyza17 <nesyaratuanastasya@gmail.com>
2025-02-23 15:22:36 +0700

    second commit

--- MahasiswaMain17.java
+++ MahasiswaMain17.java
@@ -1,55 +1,22 @@
 public class MahasiswaMain17 {
     public static void main(String[] args) {
-        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa17 mhs1 = new Mahasiswa17();
         
         mhs1.nama = "Muhammad Ali Farhan";
-        mhs1.nim = "2241720117";
+        mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
-        
+
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
-        mhs1.updateIpk(3.60);
+        mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
 
-        Mahasiswa mhs2 = new Mahasiswa("Annisa Nabila", "2141720160", "TI 2L", 3.25);
-        mhs2.updateIpk(3.30);
+        Mahasiswa17 mhs2 = new Mahasiswa17("Annisa Nabila", "2141720160", "TI 2L", 3.25);
+
+        mhs2.updateIPK(3.30);
         mhs2.tampilkanInformasi();
-    }
-}
 
-class Mahasiswa {
-    String nama;
-    String nim;
-    String kelas;
-    double ipk;
-
-    public Mahasiswa() {
-    }
-
-    public Mahasiswa(String nama, String nim, String kelas, double ipk) {
-        this.nama = nama;
-        this.nim = nim;
-        this.kelas = kelas;
-        this.ipk = ipk;
-    }
-
-    void tampilkanInformasi() {
-        System.out.println("Nama : " + nama);
-        System.out.println("NIM : " + nim);
-        System.out.println("Kelas : " + kelas);
-        System.out.println("IPK : " + ipk);
-    }
-
-    void ubahKelas(String kelasBaru) {
-        kelas = kelasBaru;
-    }
-
-    void updateIpk(double ipkBaru) {
-        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-            ipk = ipkBaru;
-        } else {
-            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }
+        Mahasiswa17 mhs3 = new Mahasiswa17("Neyza", "244107060119", "SIB 1C", 3.55);
     }
 }
